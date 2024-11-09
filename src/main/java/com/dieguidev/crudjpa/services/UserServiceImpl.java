fa4e666dd200ba1a9544a6886d47b9dcfebf8867
Dieguidev <diegogaraycullas@gmail.com>
2024-11-09 08:14:59 -0500

    Asignando roles y encriptando password con Bcrypt

--- src/main/java/com/dieguidev/crudjpa/services/UserServiceImpl.java
+++ src/main/java/com/dieguidev/crudjpa/services/UserServiceImpl.java
@@ -1,12 +1,15 @@
 package com.dieguidev.crudjpa.services;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.dieguidev.crudjpa.entities.Role;
 import com.dieguidev.crudjpa.entities.User;
 import com.dieguidev.crudjpa.repositories.RoleRepository;
 import com.dieguidev.crudjpa.repositories.UserRepository;
@@ -23,6 +26,7 @@ public class UserServiceImpl implements IUserService{
 
     @Autowired
     private PasswordEncoder passwordEncoder;
+
     @Override
     @Transactional(readOnly = true)
     public List<User> findAll() {
@@ -32,7 +36,18 @@ public class UserServiceImpl implements IUserService{
     @Override
     @Transactional
     public User save(User user) {
+        Optional<Role> optionalRoleUser = roleRepository.findByName("ROLE_USER");
+        List<Role> roles = new ArrayList<>();
 
+        optionalRoleUser.ifPresent(roles::add);
+
+        if(user.isAdmin()) {
+            Optional<Role> optionalRoleAdmin = roleRepository.findByName("ROLE_ADMIN");
+            optionalRoleAdmin.ifPresent(roles::add);
+        }
+
+        user.setRoles(roles);
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
         return userRepository.save(user);
     }
 
