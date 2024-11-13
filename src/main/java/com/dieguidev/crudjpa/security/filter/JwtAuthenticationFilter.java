b15174fc812930afb836a4c87f9f1a79e3f25c19
Dieguidev <diegogaraycullas@gmail.com>
2024-11-13 18:24:32 -0500

    Implementando Login successful & Generando e firmando el token JWT: Añadiendo soporte para JWT en el filtro de autenticación y configurando dependencias necesarias

--- src/main/java/com/dieguidev/crudjpa/security/filter/JwtAuthenticationFilter.java
+++ src/main/java/com/dieguidev/crudjpa/security/filter/JwtAuthenticationFilter.java
@@ -1,6 +1,10 @@
 package com.dieguidev.crudjpa.security.filter;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.crypto.SecretKey;
 
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -13,11 +17,16 @@ import com.fasterxml.jackson.core.exc.StreamReadException;
 import com.fasterxml.jackson.databind.DatabindException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import io.jsonwebtoken.Jwts;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilter{
+public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
     private AuthenticationManager authenticationManager;
+
+    private static final SecretKey SECRET_KEY = Jwts.SIG.HS256.key().build();
 
     public JwtAuthenticationFilter(AuthenticationManager authenticationManager) {
         this.authenticationManager = authenticationManager;
@@ -35,7 +44,7 @@ public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilte
             username = user.getUsername();
             password = user.getPassword();
         } catch (StreamReadException e) {
-            
+
             e.printStackTrace();
         } catch (DatabindException e) {
             e.printStackTrace();
@@ -43,10 +52,30 @@ public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilte
             e.printStackTrace();
         }
 
-        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(username, password);
+        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(username,
+                password);
 
         return authenticationManager.authenticate(authenticationToken);
     }
-    
-    
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain,
+            Authentication authResult) throws IOException, ServletException {
+        
+        User user = (User) authResult.getPrincipal();
+        String username = user.getUsername();
+        String token = Jwts.builder().subject(username).signWith(SECRET_KEY).compact();
+
+        response.addHeader("Authorization", "Bearer " + token);
+
+        Map<String, String> body = new HashMap<>();
+        body.put("token", token);
+        body.put("username", username);
+        body.put("message", String.format("Hola %s, has iniciado sesión con éxito", username));
+
+        response.getWriter().write(new ObjectMapper().writeValueAsString(body));
+        response.setContentType("application/json");
+        response.setStatus(200);
+    }
+
 }
