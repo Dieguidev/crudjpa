16ccda3478d3dcf28bb93a1395e88c0a2cedeb9e
Dieguidev <diegogaraycullas@gmail.com>
2024-11-13 17:50:41 -0500

    Implementando componente UserDetailsService: Agregando servicio JpaUserDetailsService y método findByUsername en UserRepository

--- src/main/java/com/dieguidev/crudjpa/repositories/UserRepository.java
+++ src/main/java/com/dieguidev/crudjpa/repositories/UserRepository.java
@@ -1,10 +1,12 @@
 package com.dieguidev.crudjpa.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.repository.CrudRepository;
 
 import com.dieguidev.crudjpa.entities.User;
 
 public interface UserRepository extends CrudRepository<User, Long> {
     boolean existsByUsername(String username);
-
+    Optional<User> findByUsername(String username);
 }
---
+++ src/main/java/com/dieguidev/crudjpa/services/JpaUserDetailsService.java
@@ -0,0 +1,41 @@
+package com.dieguidev.crudjpa.services;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.dieguidev.crudjpa.entities.User;
+import com.dieguidev.crudjpa.repositories.UserRepository;
+
+@Service
+public class JpaUserDetailsService implements UserDetailsService {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Transactional(readOnly = true)
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<User> userOptional = userRepository.findByUsername(username);
+        if (userOptional.isPresent()) {
+            throw new UsernameNotFoundException(String.format("Username %s no existe en el sistema", username));
+        }
+
+        User user = userOptional.get();
+        List<GrantedAuthority> authorities = user.getRoles().stream()
+                .map(role -> new SimpleGrantedAuthority(role.getName()))
+                .collect(Collectors.toList());
+
+        return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(),
+                user.isEnable(), true, true, true, authorities);
+    }
+
+}
